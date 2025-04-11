a2b2dbf241bfd97bcd4404943c4de947652e0ecc
sahilkhan16oct <slkn121316@gmail.com>
2025-04-11 19:18:06 +0530

    updated by sahil

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file , send_from_directory
 from flask_cors import CORS
 import Controllers.prelimLEF as lef  
 import platform
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../my-new-app/build', static_url_path='')
 CORS(app)  # Enable CORS for all routes
+
+# Route to serve React build files
+@app.route('/')
+def serve_react():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# To serve static files like JS, CSS, etc.
+@app.route('/<path:path>')
+def serve_static_file(path):
+    file_path = os.path.join(app.static_folder, path)
+    if os.path.exists(file_path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({'message': 'yes this site working'}), 200
+
 @app.route('/download', methods=['GET'])
 def download_gds():
     try:
@@ -32,4 +51,4 @@ def main():
     return jsonify({'message': 'Successfully executed'}), 200  # Success response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
